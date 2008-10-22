e18d0dcdd782c5a8de33ad3dad604a62392dd73e
Samuel Goodwin <sgoodwin@wl-dhcp143-236.Mines.EDU>
2008-10-22 10:58:43 -0600

    Fixed Transition between menu and checkout list except for hiding issue.

---
+++ Classes/LevelOneViewController.h
@@ -0,0 +1,16 @@
+//
+//  LevelOneViewController.h
+//  Food
+//
+//  Created by Samuel Goodwin on 10/21/08.
+//  Copyright 2008 Goodwinlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LevelOneViewController : UITableViewController {
+
+}
+
+@end
